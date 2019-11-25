24361d41f0980a73f21e62365e1754a4b4f46edd
Pascal <pascalvanleeuwen604@gmail.com>
2019-11-25 22:22:36 +0000

    Added assertEqual.js file

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return ("😻😻😻Assertion Passed: " + actual + " === " + expected + ".");
+  } else {
+    return ("😿😿😿Assertion Failed: " + actual + " !== " + expected + ".");
+  }
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1, '1'));
+console.log(assertEqual(76, 76));
+console.log(assertEqual('toast', 'space'));
