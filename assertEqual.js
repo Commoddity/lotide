138701d147c5a7c18075cc6e76fe3a93849d03b4
Pascal <pascalvanleeuwen604@gmail.com>
2019-11-25 22:42:23 +0000

    Testing netrc

--- assertEqual.js
+++ assertEqual.js
@@ -12,3 +12,4 @@ console.log(assertEqual(1, 1));
 console.log(assertEqual(1, '1'));
 console.log(assertEqual(76, 76));
 console.log(assertEqual('toast', 'space'));
+console.log(assertEqual('toast', 'toast'));
