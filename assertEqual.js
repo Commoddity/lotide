11aff094beff2505085777cb90b0483f4d1ada0a
Pascal <pascalvanleeuwen604@gmail.com>
2019-11-26 00:07:14 +0000

    Updated assertEqual.js file.

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return ("😻😻😻Assertion Passed: " + actual + " === " + expected + ".");
+    return (`😻😻😻Assertion Passed: ${actual} === ${expected}.`);
   } else {
-    return ("😿😿😿Assertion Failed: " + actual + " !== " + expected + ".");
+    return (`😿😿😿Assertion Failed: ${actual} !== ${expected}.`);
   }
 };
 
